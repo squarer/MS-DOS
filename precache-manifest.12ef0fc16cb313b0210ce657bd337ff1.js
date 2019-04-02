574e2b0cd17b4f7195dca14a870c889e68b12777
squarer <squarerliao@gmail.com>
2019-04-02 23:04:10 +0800

    Updates

---
+++ precache-manifest.12ef0fc16cb313b0210ce657bd337ff1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46f4681cf98a572f2ce6",
+    "url": "/MS-DOS/static/js/runtime~main.6d97d4e6.js"
+  },
+  {
+    "revision": "86805ec6bb0376d0d24f",
+    "url": "/MS-DOS/static/js/main.78895574.chunk.js"
+  },
+  {
+    "revision": "73ba973861789e50d243",
+    "url": "/MS-DOS/static/js/2.a5499f1b.chunk.js"
+  },
+  {
+    "revision": "86805ec6bb0376d0d24f",
+    "url": "/MS-DOS/static/css/main.0dd5dc86.chunk.css"
+  },
+  {
+    "revision": "2e5d454ed7c0ccf685e76ff4716f3716",
+    "url": "/MS-DOS/index.html"
+  }
+];
